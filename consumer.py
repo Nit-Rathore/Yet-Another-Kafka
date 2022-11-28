432f3694c8f44f73f229e784d12300543962ef35
Nitish <nitishrathore9@gmail.com>
2022-11-29 00:47:44 +0530

    Updated consumer to receive msg n send ACK to broker

--- consumer.py
+++ consumer.py
@@ -1,4 +1,9 @@
+#Implementing the consumer as a client architecture to pass the data from the broker to the client.
+
 import socket
+import threading 
+import sys
+import json 
 
 class consumer():
 	DEFAULT_CONFIG = {
@@ -9,36 +14,41 @@ class consumer():
 		'server': 'localhost'
 	}
 	DEFAULT_CONFIG['ADDR'] = (DEFAULT_CONFIG['server'], DEFAULT_CONFIG['port'])
+	data = {'type' : 'Consumer', 'topic': None, 'flag': 0} #Not-beginning == 0, -from-beginning ==1 
 
-	def __init__(self):
-		pass
+	def __init__(self,topic=None,flag=None):
+		self.topic = topic
+		self.flag = flag
 
 	def connect(self):
 		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.client.connect(self.DEFAULT_CONFIG['ADDR'])
-	
-	def send(self, msg):
+		 
+	def recieve(self, topic,msg):  ##To send the broker my data received from user and receive data from broker accordingly...
+		self.data.update({'topic':self.topic, 'flag':self.flag})
+		data_string = str.encode(json.dumps(self.data)) #data serialized
+		#data_loaded = json.loads(self.data) #Not sure if this is required or not ...
+		self.client.sendall(data_string) #Sending the metadata to the broker ....
+		message = self.client.recv(2048).decode(self.DEFAULT_CONFIG['format'])
+		if(len(message)): print(message) , self.client.send("Message recieved from leader broker".encode())
+
+		'''
 		message = msg.encode(self.DEFAULT_CONFIG['format'])
 		self.client.send(str(len(message)).encode(self.DEFAULT_CONFIG['format']) + b' '*(self.DEFAULT_CONFIG['header'] - len(str(len(message)).encode(self.DEFAULT_CONFIG['format']))))
 		self.client.send(message)
 		ack = self.client.recv(2048).decode(self.DEFAULT_CONFIG['format'])
-		print(ack)
+		print(ack)'''
 
 	def disconnect(self):
 		self.send(self.DEFAULT_CONFIG['disconnect_msg'])
 		self.client.close()
+		
 
-
-if __name__ == '__main__':
-	cons1 = consumer()
+if __name__ == "__main__":
+    
+	topic = sys.argv[1]
+	flag = sys.argv[2]
+	cons1 = consumer(topic,flag)
 	cons1.connect()
-	message = 'consumer'
-	cons1.send(message)
-	print("Testing github stuff")
-	cons2 = consumer()
-	cons2.connect()
-	message = 'cons2 HELLO'
-	cons2.send(message)
-
 	cons1.disconnect()
-	cons2.disconnect()
+	
