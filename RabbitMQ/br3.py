5b4705d2d2a725a375890229d30481effba16741
Yashas R Kashyap <81970610+KetchupCodes@users.noreply.github.com>
2022-11-30 11:25:19 +0530

    Added Dynamic Partitioning and 3 fold replication

--- RabbitMQ/br3.py
+++ RabbitMQ/br3.py
@@ -1,39 +1,64 @@
 import pika
 from pika.exchange_type import ExchangeType
 import os
-from zookeeper_rabbit import get_leadership_info
-
+from redis import Redis
+import json
+cli = Redis('localhost')
 connection_parameters = pika.ConnectionParameters('localhost')
 connection = pika.BlockingConnection(connection_parameters)
+
 channel = connection.channel()
-channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
 queue = channel.queue_declare(queue='', exclusive=True)
+channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
+channel.queue_bind(exchange='routing',queue=queue.method.queue,routing_key='zookeeper')
+
+
+channel2 = connection.channel()
+queue2 = channel.queue_declare(queue='', exclusive=True)
+channel2.exchange_declare(exchange='routing2', exchange_type=ExchangeType.direct)
+
 
 
 def open_required_queues():    
-    lino = get_leadership_info()
+    lino = json.loads(cli.get('leadership')) 
     for topic in lino:
         num_part = lino[topic][0]
         for i in range(1,num_part+1):
-            if(lino[topic][i]==2):
-                channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key=f'{topic}/partition{i}')   
-    return channel,queue
-
+            if(lino[topic][i]==3):
+                channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key=f'3/{topic}/partition{i}')   
+            else:
+                channel2.queue_bind(exchange='routing2', queue=queue2.method.queue, routing_key=f'3/{topic}/partition{i}')
+                
+                
 
 def on_message_received(ch, method, properties, body): 
-    print(f"Third Broker - received new message: {body}")
-    os.makedirs(f'br3/{method.routing_key}', exist_ok=True)
-    f = open(f"{method.routing_key}/log.txt", "w")
-    f.write(f"{body}")
-    f.close()
+    if(method.routing_key=="zookeeper"):
+        print(f"Broker - received new message: {body}")
+        open_required_queues()
+    else:
+        
+            print(f"Broker - received new message: {body}")
+            os.makedirs(f'{method.routing_key}', exist_ok=True)
+            f = open(f"{method.routing_key}/log.txt", "a")
+            f.write(str(body))
+            f.close() 
+            channel2.basic_publish(exchange='routing2', routing_key="1"+method.routing_key[1:], body=str(body))
+            channel2.basic_publish(exchange='routing2', routing_key="2"+method.routing_key[1:], body=str(body))
+        
 
-
-
-open_required_queues()
+def omr(ch, method, properties, body):
+            os.makedirs(f'{method.routing_key}', exist_ok=True)
+            f = open(f"{method.routing_key}/log.txt", "a")
+            f.write(str(body))
+            f.close() 
 
 channel.basic_consume(queue=queue.method.queue, auto_ack=True,
     on_message_callback=on_message_received)
 
+channel2.basic_consume(queue=queue2.method.queue, auto_ack=True,
+    on_message_callback=omr)
+
 print("Broker 3 running")
 
 channel.start_consuming()
+channel2.start_consuming()
--- RabbitMQ/producer_class.py
+++ RabbitMQ/producer_class.py
@@ -1,6 +1,10 @@
 import pika
 from pika.exchange_type import ExchangeType
 from zookeeper_rabbit import set_leader,get_leader
+from redis import Redis
+import json
+cli = Redis('localhost')
+
 class producer():
     def createTopic(topicName,num_partition):
        set_leader(topicName=topicName,num_part=num_partition)
@@ -11,13 +15,12 @@ class producer():
        channel = connection.channel()
        channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
        queue = channel.queue_declare(queue='', exclusive=True)
-       channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key=topicName)
        return connection,channel
     
     def publish(topicName,key,value,channel):
-       leader = get_leader(key)
-       routing_queue_name = f'{topicName}/partition{leader}'
-       print(f'sent message: {key}:{value}')
+       part,leader = get_leader(topicName,key)
+       routing_queue_name = f'{leader}/{topicName}/partition{part}'
+       print(f'sent message: {key}:{value} {routing_queue_name}')
        channel.basic_publish(exchange='routing', routing_key=routing_queue_name, body=f"{key}:{value}")
        
     def close(connection):
